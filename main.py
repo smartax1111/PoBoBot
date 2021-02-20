5f316ac5fec7975a101a0b8768aa20e14438d6f1
TJ Uhler <terryuhler@gmail.com>
2021-02-20 02:17:15 +0000

    v0.4 now with better messages and console messages

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ bot = commands.Bot(command_prefix="$")
 client = discord.Client()
 
 
-response = requests.get("https://opentdb.com/api.php?amount=10&type=boolean")
+
+response = requests.get("https://opentdb.com/api.php?amount=50&type=boolean")
 response_data = json.loads(response.text)
 global question_answer
 
@@ -33,7 +34,7 @@ def answer_not_allowed():
   question_answer = False
 
 def get_question():
-  randQ = random.randrange(10)
+  randQ = random.randrange(50)
   question = (response_data['results'][randQ]['question'])
   
   return question, randQ;
@@ -47,20 +48,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  user = message.author
+  #keys = db.keys()
   question, randQ = get_question()
   correct_answer = (response_data['results'][randQ]['correct_answer'])
-
-  if message.author == client.user: 
+  incorrect_answer = (response_data['results'][randQ]['incorrect_answers'][0])
+  
+  if user == client.user: 
     return
 
-  user = message.author
+  
 
-  if message.content.startswith("Q"):
+  if message.content.startswith("Q"): 
+    print(str(user) + ' has asked for a question')
+
     global question_answer
     question_answer = True
-
-    myEmbed = discord.Embed(title = "Random Question Alpha V0.3", description = str(question) + ' #' + str(randQ), color = 0x00ff00)
-    myEmbed.add_field(name = 'Question Bot PoBoBot', value = 'v0.3.5', inline = False)
+    
+    myEmbed = discord.Embed(title = "Random Question Alpha V0.4", description = str(question) + ' #' + str(randQ), color = 0x00ff00)
+    myEmbed.add_field(name = 'Question Bot PoBoBot', value = 'v0.4 now with better messages and console messages', inline = False)
     myEmbed.add_field(name = 'Date Edited:', value = 'February 19th, 2021', inline = False)
     myEmbed.set_footer(text = "Please answer 'True' or 'False'")
 
@@ -68,18 +74,27 @@ async def on_message(message):
 
 
   elif message.content == correct_answer and question_answer == True:
-    response = 'That was correct'
+    response = 'That was correct. +1 point rewarded!'
     plus_one(user)
-    print(db[user.id])
+    print(str(user) + ' now has ' + str(db[user.id]) + ' points')
+    answer_not_allowed()
+    await message.channel.send(response)
+
+  elif message.content == incorrect_answer and question_answer == True:
+    response = 'That was incorrect. reduced 1 point :('
+    minus_one(user)
+    print(str(user) + ' now has ' + str(db[user.id]) + ' points')
     answer_not_allowed()
     await message.channel.send(response)
 
   elif message.content == '$points':
-    print('user has checked points')
-    await message.channel.send('You have ' + str(db[user.id]) + ' points!')
+    print(str(user) + ' has checked their points')
+    await message.channel.send(str(user) + ' has ' + str(db[user.id]) + ' points!')
 
-  elif message.content == '$leaderboard':
-    
+  #elif message.content == '$leaderboard':
+    #print('user has checked leaderboard')
+    #await message.channel.send(str(keys))
+
 
 client.run(os.getenv("TOKEN"))
 
